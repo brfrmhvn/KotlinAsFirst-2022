a13e2a4fbf177c2e6d27e3ec8067a7c2b0a83de8
HP <p.samohvalova03@gmail.com>
2022-11-13 15:37:12 +0300

    Map. Решены containsIn, mergePhoneBooks. Решена extractRepeats. Исправлена extractRepeats Parse. Решены dateStrToDigit, dateDigitToStr. Исправлены dateStrToDigit, dateDigitToStr

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -96,18 +96,7 @@ fun buildWordSet(text: List<String>): MutableSet<String> {
  *   buildGrades(mapOf("Марат" to 3, "Семён" to 5, "Михаил" to 5))
  *     -> mapOf(5 to listOf("Семён", "Михаил"), 3 to listOf("Марат"))
  */
-fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO() /*{
-    val gradestud: MutableMap<Int, List<String>> = mutableMapOf<Int, List<String>>()
-    for ((name, grade) in grades) {
-        val stud = mutableListOf<String>()
-            if (grade == i) {
-                stud.add(name)
-            }
-        }
-        if (stud.size > 0) gradestud[i] = stud
-    }
-    return gradestud
-}*/
+fun buildGrades(grades: Map<String, Int>): Map<Int, List<String>> = TODO()
 
 /**
  * Простая (2 балла)
