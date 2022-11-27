83ba9130571b22012245fc8b1488cee0a39c0fcf
HP <p.samohvalova03@gmail.com>
2022-11-27 16:40:10 +0300

    Map. Решена averageStockPrice, canBuildFrom

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -211,7 +211,7 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean = when {
-    word.isEmpty() -> true
+    word.length == 1 && chars.contains(word.toCharArray()[0]) || word.isEmpty()-> true
     chars.isNotEmpty() -> word.toList().containsAll(chars)
     else -> false
 }
