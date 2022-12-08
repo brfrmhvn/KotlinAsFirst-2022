a9e8290968c87854188735c84276da53a56fb834
HP <p.samohvalova03@gmail.com>
2022-12-08 19:31:07 +0300

    Fixes

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -340,3 +340,4 @@ fun findSumOfTwo(list: List<Int>, number: Int): Pair<Int, Int> {
  *     450
  *   ) -> emptySet()
  */
+fun bagPacking(treasures: Map<String, Pair<Int, Int>>, capacity: Int): Set<String> = TODO()
