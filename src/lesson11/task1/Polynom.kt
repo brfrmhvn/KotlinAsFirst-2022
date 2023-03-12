7acf258e9f0558b5c14fb89aa766dad8c2236734
HP <p.samohvalova03@gmail.com>
2023-03-12 11:43:58 +0300

    Polynom, PhoneBook

--- src/lesson11/task1/Polynom.kt
+++ src/lesson11/task1/Polynom.kt
@@ -2,6 +2,9 @@
 
 package lesson11.task1
 
+import java.util.*
+import kotlin.math.pow
+
 /**
  * Класс "полином с вещественными коэффициентами".
  *
@@ -20,16 +23,26 @@ package lesson11.task1
  * Старшие коэффициенты, равные нулю, игнорировать, например Polynom(0.0, 0.0, 5.0, 3.0) соответствует 5x+3
  */
 class Polynom(vararg coeffs: Double) {
+    private val polynom: List<Double> = when {
+        coeffs.isEmpty() || coeffs.size == 1 && coeffs.first() == 0.0 -> listOf(0.0)
+        else -> {
+            val result = coeffs.reversed().toMutableList()
+            while (result.last() == 0.0) {
+                result.remove(result.last())
+            }
+            result.toList()
+        }
+    }
 
     /**
      * Геттер: вернуть значение коэффициента при x^i
      */
-    fun coeff(i: Int): Double = TODO()
+    fun coeff(i: Int): Double = polynom.getOrNull(i) ?: throw NoSuchElementException()
 
     /**
      * Расчёт значения при заданном x
      */
-    fun getValue(x: Double): Double = TODO()
+    fun getValue(x: Double): Double = polynom.indices.sumOf { x.pow(it) * coeff(it) }
 
     /**
      * Степень (максимальная степень x при ненулевом слагаемом, например 2 для x^2+x+1).
@@ -38,27 +51,49 @@ class Polynom(vararg coeffs: Double) {
      * Слагаемые с нулевыми коэффициентами игнорировать, т.е.
      * степень 0x^2+0x+2 также равна 0.
      */
-    fun degree(): Int = TODO()
+    fun degree(): Int = polynom.size - 1
 
     /**
      * Сложение
      */
-    operator fun plus(other: Polynom): Polynom = TODO()
+    operator fun plus(other: Polynom): Polynom {
+        val result = mutableListOf<Double>()
+        if (polynom.size < other.polynom.size) {
+            for (i in other.polynom.indices) {
+                result.add(polynom.getOrElse(i) { 0.0 } + other.polynom.getOrElse(i) { 0.0 })
+            }
+        } else {
+            for (i in polynom.indices) {
+                result.add(polynom.getOrElse(i) { 0.0 } + other.polynom.getOrElse(i) { 0.0 })
+            }
+        }
+        return Polynom(*result.reversed().toDoubleArray())
+    }
 
     /**
      * Смена знака (при всех слагаемых)
      */
-    operator fun unaryMinus(): Polynom = TODO()
+    operator fun unaryMinus(): Polynom = Polynom(*polynom.map { -it }.reversed().toDoubleArray())
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Polynom): Polynom = TODO()
+    operator fun minus(other: Polynom): Polynom = plus(other.unaryMinus())
 
     /**
      * Умножение
      */
-    operator fun times(other: Polynom): Polynom = TODO()
+    operator fun times(other: Polynom): Polynom {
+        val result = mutableMapOf<Int, Double>()
+        for (i in polynom.indices) {
+            for (k in other.polynom.indices) {
+                if (result.containsKey(i + k)) result[i + k] =
+                    polynom[i] * other.polynom[k] + result.getValue(i + k) else result[i + k] =
+                    polynom[i] * other.polynom[k]
+            }
+        }
+        return Polynom(*result.values.reversed().toDoubleArray())
+    }
 
     /**
      * Деление
@@ -68,20 +103,42 @@ class Polynom(vararg coeffs: Double) {
      *
      * Если A / B = C и A % B = D, то A = B * C + D и степень D меньше степени B
      */
-    operator fun div(other: Polynom): Polynom = TODO()
+    operator fun div(other: Polynom): Polynom {
+        val dividend = polynom.toMutableList()
+        val divisor = other.polynom.toMutableList()
+        val result = MutableList(dividend.size - divisor.size + 1) { 0.0 }
+        while (dividend.size >= divisor.size) {
+            val ratio = dividend.last() / divisor.last()
+            val degree = dividend.size - divisor.size
+            result[degree] = ratio
+            for (i in 0 until divisor.size) {
+                dividend[degree + i] -= ratio * divisor[i]
+            }
+            dividend.removeLast()
+        }
+        return Polynom(*result.reversed().toDoubleArray())
+    }
 
     /**
      * Взятие остатка
      */
-    operator fun rem(other: Polynom): Polynom = TODO()
+    operator fun rem(other: Polynom): Polynom =
+        if (polynom.size < other.polynom.size) Polynom(*polynom.toDoubleArray()) else Polynom(
+            *(this - other * (this / other)).polynom.reversed().toDoubleArray()
+        )
+
+    /**
+     * Преобразование в строку
+     */
+    //override fun toString(): String = "Polynom(${polynom.toList()}"
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Polynom && polynom == other.polynom
 
     /**
      * Получение хеш-кода
      */
-    override fun hashCode(): Int = TODO()
+    override fun hashCode(): Int = polynom.hashCode()
 }
--- src/lesson12/task1/PhoneBook.kt
+++ src/lesson12/task1/PhoneBook.kt
@@ -18,13 +18,19 @@ package lesson12.task1
  * Класс должен иметь конструктор по умолчанию (без параметров).
  */
 class PhoneBook {
+    private val phoneNumbers = mutableMapOf<String, String>() // phone to person
+    private val names = mutableMapOf<String, MutableSet<String>>() //person to number of phones
+
     /**
      * Добавить человека.
      * Возвращает true, если человек был успешно добавлен,
      * и false, если человек с таким именем уже был в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun addHuman(name: String): Boolean = TODO()
+    fun addHuman(name: String): Boolean = if (names.containsKey(name)) false else {
+        names[name] = mutableSetOf()
+        true
+    }
 
     /**
      * Убрать человека.
@@ -32,7 +38,10 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * (во втором случае телефонная книга не должна меняться).
      */
-    fun removeHuman(name: String): Boolean = TODO()
+    fun removeHuman(name: String): Boolean = if (!names.containsKey(name)) false else {
+        names.remove(name)
+        true
+    }
 
     /**
      * Добавить номер телефона.
@@ -41,7 +50,12 @@ class PhoneBook {
      * либо у него уже был такой номер телефона,
      * либо такой номер телефона зарегистрирован за другим человеком.
      */
-    fun addPhone(name: String, phone: String): Boolean = TODO()
+    fun addPhone(name: String, phone: String): Boolean =
+        if (phoneNumbers.containsKey(phone) || !names.containsKey(name)) false else {
+            phoneNumbers[phone] = name
+            names.getValue(name).add(phone)
+            true
+        }
 
     /**
      * Убрать номер телефона.
@@ -49,24 +63,35 @@ class PhoneBook {
      * и false, если человек с таким именем отсутствовал в телефонной книге
      * либо у него не было такого номера телефона.
      */
-    fun removePhone(name: String, phone: String): Boolean = TODO()
+    fun removePhone(name: String, phone: String): Boolean =
+        if (!phoneNumbers.containsKey(phone) || !names.containsKey(name)) false else {
+            phoneNumbers.remove(phone)
+            names.getValue(name).remove(phone)
+            true
+        }
 
     /**
      * Вернуть все номера телефона заданного человека.
      * Если этого человека нет в книге, вернуть пустой список
      */
-    fun phones(name: String): Set<String> = TODO()
+    fun phones(name: String): Set<String> = if (!names.containsKey(name)) emptySet<String>() else names.getValue(name)
 
     /**
      * Вернуть имя человека по заданному номеру телефона.
      * Если такого номера нет в книге, вернуть null.
      */
-    fun humanByPhone(phone: String): String? = TODO()
+    fun humanByPhone(phone: String): String? =
+        if (!phoneNumbers.containsKey(phone)) null else phoneNumbers.getValue(phone)
 
     /**
      * Две телефонные книги равны, если в них хранится одинаковый набор людей,
      * и каждому человеку соответствует одинаковый набор телефонов.
      * Порядок людей / порядок телефонов в книге не должен иметь значения.
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is PhoneBook && names == other.names
+    override fun hashCode(): Int {
+        var result = phoneNumbers.hashCode()
+        result = 31 * result + names.hashCode()
+        return result
+    }
 }
